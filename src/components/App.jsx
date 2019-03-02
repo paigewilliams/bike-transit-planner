2d865baea757fb587d4388e2d199fa3e179ad917
Paige Williams <paw145@humboldt.edu>
2019-03-02 12:13:12 -0800

    Create grid for App.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,27 @@
 import React from 'react';
-import styled from 'styled-components';
 import Map from './Map';
 import Header from './Header';
 import Sidebar from './Sidebar';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  display: grid;
+  grid-template-rows: 15% auto;
+  grid-template-columns: 33% auto;
+  grid-gap: 1rem;
+  grid-template-areas: 
+    "header"
+    "sidebar"
+    "map"
+`
 
 function App() {
   return(
-    <div>
+    <Container>
       <Header />
       <Sidebar />
       <Map />
-    </div>
+    </Container>
   );
 }
 

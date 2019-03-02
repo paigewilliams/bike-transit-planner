ab43c6ed8b09f4e987b37ca35497b283bc31c439
Paige Williams <paw145@humboldt.edu>
2019-03-01 18:03:38 -0800

    Arrange components in tree.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
-import styled from 'styled-components'
+import styled from 'styled-components';
+import Map from './Map';
+import Header from './Header';
+import Sidebar from './Sidebar';
 
 const Title = styled.h1`
   font-size: 1.5em;
@@ -11,6 +14,9 @@ function App() {
   return(
     <div>
       <Title>App works!</Title>
+      <Header />
+      <Sidebar />
+      <Map />
     </div>
   );
 }
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,9 +1,11 @@
-import react from 'React';
+import React from 'react';
+import RouteForm from './RouteForm';
 
 function Sidebar(){
   return(
     <div>
       <h1>Sidebar works</h1>
+      <RouteForm />
     </div>
   )
 }

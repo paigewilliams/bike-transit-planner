aa52879479fc40a9211af112947fb32d93714003
Paige Williams <paw145@humboldt.edu>
2019-03-01 16:36:19 -0800

    Environment builds.

--- webpack.config.js
+++ webpack.config.js
@@ -46,6 +46,10 @@ module.exports = {
         loader: "babel-loader",
         exclude: /node_modules/,
         options: {
+          presets: [
+            ["es2015", {"modules": false}],
+            "react",
+          ],
           plugins: [
             "react-hot-loader/babel"
           ]

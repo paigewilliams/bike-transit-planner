3089dd7b1ff8925de1240fea81c117a3fc6c434e
Paige Williams <paw145@humboldt.edu>
2019-03-01 16:39:21 -0800

    Add styled component; working.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import styled from 'styled-components'
+
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`
 
 function App() {
   return(
     <div>
-      <h1>App works!</h1>
+      <Title>App works!</Title>
     </div>
   );
 }

116db973810ac6e7547f678b019f1a3529103542
Paige Williams <paw145@humboldt.edu>
2019-03-02 13:06:54 -0800

    Add Timefield package and inputs for form.

--- src/components/RouteForm.jsx
+++ src/components/RouteForm.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import TimeField from 'react-simple-timefield';
 
 function RouteForm(){
   let _startAddress = null;
   let _endAddress = null;
-
+  let _departOrArrive = null;
+  let _date = null;
+  let _miles = null;
   return(
     <div>
       <form>
@@ -17,6 +20,21 @@ function RouteForm(){
           id='end'
           placeholder='Destination Address'
           ref={(input)=> {_endAddress = input}}/>
+          <select ref={(option)=> {_departOrArrive = option}}>
+            <option value="depart">Depart</option>
+            <option value ="arrive">Arrive</option>
+          </select>
+          <p>by</p>
+          <TimeField />
+          <input
+            type='date'
+            id='date'
+            ref={(input)=> {_date = input}}/>
+           <input 
+            type='number'
+            id='miles'
+            placeholder='Maximum miles biked'
+            ref={(input)=> {_miles = input}}/> 
        <button type='submit'>Find Route</button>
 
       </form>

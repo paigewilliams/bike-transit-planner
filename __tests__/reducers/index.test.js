dbf33ede96f836f2bb71488dbba70744e46ba6d3
Paige Williams <paw145@humboldt.edu>
2019-03-07 09:32:22 -0800

    Write default test for root reducer; passing.

--- __tests__/reducers/index.test.js
+++ __tests__/reducers/index.test.js
@@ -1,4 +1,15 @@
 import searchParamsReducer from './../../src/reducers/search-params-reducer';
+import rootReducer from './../../src/reducers/index';
+
+describe('rootReducer', () => {
+
+  test('should return default state if no action type is recognized', () => {
+    expect(rootReducer({}, { type: null })).toEqual({
+      searchParams: {}
+    });
+  });
+});
+
 
 describe('searchParamsReducer', () => {
 

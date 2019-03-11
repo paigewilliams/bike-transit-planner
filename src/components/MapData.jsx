b3371d441c6a505df7282d808142cd7136f28a60
Paige Williams <paw145@humboldt.edu>
2019-03-11 16:48:53 -0700

    Begin converting geojsons to feature collection; not working.

--- src/components/MapData.jsx
+++ src/components/MapData.jsx
@@ -5,28 +5,25 @@ import { connect } from 'react-redux';
 function MapData(props){
 
 
-  // if (props.itineraries !== {}) {
-  //   let allLines = {};
-  //   Object.keys(props.itineraries).map(function(itineraryKey) {
-  //     const itinerary = props.itineraries[itineraryKey]
-  //     Object.keys(itinerary).map(function(key){
-  //       const legs = itinerary[key];
-  //       legs.forEach(function(leg){
-  //         Object.keys(leg).map(function(key){
-  //           const miniLeg = leg[key]
-  //           const newLine = polyline.toGeoJSON(miniLeg.legGeometry);
-  //           allLines = Object.assign({}, {}, newLine)
-  //         })
-  //       })
-  //     })
-  //   })
-  //   console.log(allLines);
+  if (props.itineraries !== {}) {
+    let allLines = {};
+    Object.keys(props.itineraries).map(function(itineraryKey) {
+      const itinerary = props.itineraries[itineraryKey]
+      Object.keys(itinerary).map(function(key){
+        const legs = itinerary[key];
+        legs.forEach(function(leg){
+            const newLine = polyline.toGeoJSON(leg.legGeometry);
+            allLines = Object.assign({}, allLines, newLine)
+        })
+      })
+    })
+    console.log(allLines);
     // const collection = JSON.stringify({
-    //   features: allLines.map(JSON.parse),
+    //   features: Object.keys(allLines).map(JSON.parse),
     //   type: 'FeatureCollection'
     // });
     // console.log(collection);
-  // }
+  }
 
   return(
     <div>

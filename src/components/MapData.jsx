4ce01e03862fc128b85247f8bc0f3d2acb24f6e0
Paige Williams <paw145@humboldt.edu>
2019-03-11 16:38:28 -0700

    Convert coordinates to geojson.

--- src/components/MapData.jsx
+++ src/components/MapData.jsx
@@ -6,6 +6,7 @@ function MapData(props){
 
 
   if (props.itineraries !== {}) {
+    let allLines = {};
     Object.keys(props.itineraries).map(function(itineraryKey) {
       const itinerary = props.itineraries[itineraryKey]
       Object.keys(itinerary).map(function(key){
@@ -13,12 +14,18 @@ function MapData(props){
         legs.forEach(function(leg){
           Object.keys(leg).map(function(key){
             const miniLeg = leg[key]
-            const newLine = polyline.decode(miniLeg.legGeometry);
-            console.log(newLine);
+            const newLine = polyline.toGeoJSON(miniLeg.legGeometry);
+            allLines = Object.assign({}, {}, newLine)
           })
         })
       })
     })
+    console.log(allLines);
+    // const collection = JSON.stringify({
+    //   features: allLines.map(JSON.parse),
+    //   type: 'FeatureCollection'
+    // });
+    // console.log(collection);
   }
 
   return(

5462d2537b29cd6eb63190d36a5abc887ef2615a
Paige Williams <paw145@humboldt.edu>
2019-03-12 09:35:03 -0700

    Add test data to map.

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -38,6 +38,47 @@ class Map extends React.Component {
         zoom: map.getZoom().toFixed(2)
       });
     });
+
+    map.on('load', () => {
+      map.addLayer({
+        "id": "route",
+        "type": "line",
+        "source": {
+          "type": "geojson",
+          "data": {
+            "type": "Feature",
+            "properties": {},
+            "geometry": {
+              "type": "LineString",
+              "coordinates": [
+                [-122.42665, 45.50247],
+                [-122.42697, 45.50254],
+                [-122.42698, 45.5025],
+                [-122.42662, 45.50243],
+                [-122.42632, 45.5024],
+                [-122.42633, 45.50244],
+                [-122.42631, 45.50248],
+                [-122.42632, 45.50255],
+                [-122.42633, 45.50262],
+                [-122.42633, 45.50268],
+                [-122.42633, 45.50285],
+                [-122.42635, 45.50289],
+                [-122.42634, 45.50293],
+                [-122.42684, 45.50293]
+              ]
+            }
+          }
+        },
+        "layout": {
+          "line-join": "round",
+          "line-cap": "round"
+          },
+          "paint": {
+          "line-color": "#888",
+          "line-width": 8
+          }
+      });
+    });
   }
 
   render(){
